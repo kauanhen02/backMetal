6ab9d57038e9e0919c91ad7919630d27ae1eb414
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2024-04-01 11:47:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.post('/enviar-email', (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'outlook',
     auth: {
-      user: 'kauanhen06@putlook.com',
+      user: 'kauanhen06@outlook.com',
       pass: 'Montanha12'
     }
   });
 
   // Configuração do e-mail a ser enviado
   const mailOptions = {
-    from: 'kauanhen06@putlook.com',
+    from: 'kauanhen06@outlook.com',
     to: 'email-que-deseja-receber-os-formularios@gmail.com',
     subject: 'Formulário de Contato',
     html: `
